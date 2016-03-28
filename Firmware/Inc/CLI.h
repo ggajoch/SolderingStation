0aa6a2caf60f2a62e3affb72268d1dfa2c30e20c
Grzegorz Gajoch <gajoch@gmail.com>
2016-03-28 02:26:22 +0200

    formatting of CLI before refactor

--- Firmware/Inc/CLI.h
+++ Firmware/Inc/CLI.h
@@ -7,58 +7,62 @@
 #include <stdint.h>
 
 struct Command {
-	char * name;
-	void (*callback)(float * params, uint8_t nrOfParams);
+    char *name;
 
-	bool operator==(const Command & rhs) const {
-		if( strcmp(this->name, rhs.name) == 0 ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    void (*callback)(float *params, uint8_t nrOfParams);
+
+    bool operator==(const Command &rhs) const {
+        if (strcmp(this->name, rhs.name) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 class CLI {
     size_t nrOfCommands;
 public:
-    CLI(size_t nrOfCommands, const Command* commands, void (*writer)(const char *)) : nrOfCommands(nrOfCommands), commands(commands), writer(writer) {
+    CLI(size_t nrOfCommands, const Command *commands, void (*writer)(const char *)) : nrOfCommands(nrOfCommands),
+                                                                                      commands(commands),
+                                                                                      writer(writer) {
 
     }
+
     const Command *commands;
 
     void (*writer)(const char *);
 
-	void parse_line(const char * line) {
-		size_t len = strlen(line);
-		uint8_t iter = 0;
-		float params[10];
-		while(line[iter] != ' ' && line[iter] != 0) ++iter;
+    void parse_line(const char *line) {
+        size_t len = strlen(line);
+        uint8_t iter = 0;
+        float params[10];
+        while (line[iter] != ' ' && line[iter] != 0) ++iter;
 
-		for(uint8_t cmd_nr = 0; cmd_nr < this->nrOfCommands; ++cmd_nr) {
-			if( strncmp(this->commands[cmd_nr].name, line, iter-1) == 0 ) {
-				// found command
-				uint8_t param_nr = 0;
-				while(iter < len) {
-					iter++;
-					char * stop;
-					float val = strtof(line+iter, &stop);
-					params[param_nr++] = val;
-					iter = stop-line;
-				}
-				commands[cmd_nr].callback(params, param_nr);
-			}
-		}
-	}
+        for (uint8_t cmd_nr = 0; cmd_nr < this->nrOfCommands; ++cmd_nr) {
+            if (strncmp(this->commands[cmd_nr].name, line, iter - 1) == 0) {
+                // found command
+                uint8_t param_nr = 0;
+                while (iter < len) {
+                    iter++;
+                    char *stop;
+                    float val = strtof(line + iter, &stop);
+                    params[param_nr++] = val;
+                    iter = stop - line;
+                }
+                commands[cmd_nr].callback(params, param_nr);
+            }
+        }
+    }
 
-    void sendCommand(Command command, float * params, uint8_t params_nr) {
+    void sendCommand(Command command, float *params, uint8_t params_nr) {
         char buf[100];
         uint8_t iter = 0;
         strcpy(buf, command.name);
         iter += strlen(command.name);
-        while( params_nr-- ) {
+        while (params_nr--) {
             buf[iter++] = ' ';
-            iter += sprintf(buf+iter, "%f", *params);
+            iter += sprintf(buf + iter, "%f", *params);
             params++;
         }
         buf[iter++] = 0;

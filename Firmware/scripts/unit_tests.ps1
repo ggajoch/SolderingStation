cb189dc45e3e78e2823d447de839e7189802d42a
Grzegorz Gajoch <gajoch@gmail.com>
2016-12-29 13:33:56 +0100

    Eclipse project for unit tests

--- Firmware/scripts/unit_tests.ps1
+++ Firmware/scripts/unit_tests.ps1
@@ -3,7 +3,7 @@ cd ..\..\
 mkdir -f build
 mkdir -f build\x86
 cd build\x86
-cmake -G "MinGW Makefiles" ..\..\Firmware\ -DTARGET_PLATFORM:STRING=x86
+cmake -G "Eclipse CDT4 - MinGW Makefiles" ..\..\Firmware\ -DTARGET_PLATFORM:STRING=x86
 make -j5
 & ".\build\x86\bin\unit_tests"
 cd $path

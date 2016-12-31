2bc0e1453f1481d296749b3a2407bfcd3c85b9d2
Piotr Rzeszut <piotrva@wp.pl>
2016-12-31 21:38:56 +0100

    initial files for memory driver

---
+++ Firmware/targets/arm/Inc/i2c_memory.h
@@ -0,0 +1,9 @@
+#ifndef TARGETS_ARM_INC_I2C_MEMORY_H_
+#define TARGETS_ARM_INC_I2C_MEMORY_H_
+
+#include <stdint.h>
+#include "stm32f1xx_hal.h"
+#include "main.h"
+#include "i2c.h"
+
+#endif  // TARGETS_ARM_INC_I2C_MEMORY_H_

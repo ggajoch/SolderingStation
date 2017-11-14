5e7d845b1d9bce8b60aa3dbb65dcf4f00e701418
Grzegorz Gajoch <gajoch@gmail.com>
2017-11-14 03:41:57 +0000

    First basic version based on PyQt, PyQtGraph and socket to simulator

---
+++ Software/PyGUI/main.py
@@ -0,0 +1,133 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from main_window import Ui_MainWindow
+import pyqtgraph as pg
+import socket
+import time
+
+
+class StartQT4(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.history_seconds = 1e10
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        QtCore.QObject.connect(self.ui.sendButton, QtCore.SIGNAL("clicked()"), self.send)
+        QtCore.QObject.connect(self.ui.actionSimulator, QtCore.SIGNAL("triggered()"), self.connect_socket)
+        QtCore.QObject.connect(self.ui.actionDisconnect, QtCore.SIGNAL("triggered()"), self.disconnect_socket)
+
+        self.curve = self.ui.widget.getPlotItem().plot()
+
+        menu = self.ui.widget.getPlotItem().getViewBox().menu
+        clear = QtGui.QAction("Clear", menu)
+        QtCore.QObject.connect(clear, QtCore.SIGNAL("triggered()"), self.clear)
+        menu.addAction(clear)
+
+        x = menu.ctrl
+        x = x[0]
+
+
+        ui = menu.ctrl[0]
+
+        ui.autoPercentSpin.valueChanged.disconnect()
+
+        ui.autoPercentSpin.setMaximum(10000)
+        ui.autoPercentSpin.setSuffix(" s")
+
+        QtCore.QObject.connect(ui.autoPercentSpin, QtCore.SIGNAL("valueChanged(int)"), self.set_history)
+        ui.autoPercentSpin.setValue(10000)
+
+        self.clear()
+        self.connect_socket()
+
+    def set_history(self, seconds):
+        self.history_seconds = seconds
+
+    def clear(self):
+        self.start_time = time.time()
+        self.x = []
+        self.y = []
+        self.curve.setData(self.x, self.y)
+
+    def getMenu(self):
+        """
+        Create the menu
+        """
+        self.menu = None
+        if self.menu is None:
+            self.menu = QtGui.QMenu("Clear")
+            self.viewAll = QtGui.QAction("Dupa", self.menu)
+            self.menu.addAction(self.viewAll)
+        return self.menu
+
+    def add_point(self, y):
+        now = time.time() - self.start_time
+
+        remove_until = 0
+        for index, value in enumerate(self.x):
+            if now - value < self.history_seconds:
+                remove_until = index
+                break
+
+        self.x[:remove_until] = []
+        self.y[:remove_until] = []
+
+        now = round(now, 2)
+        self.x.append(now)
+        self.y.append(y)
+        self.curve.setData(self.x, self.y)
+
+    class UpdatePlotThread(QtCore.QThread):
+        def __init__(self, add_point):
+            QtCore.QThread.__init__(self)
+            self.add_point = add_point
+
+        def run(self):
+            print "Connectting to TCP"
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(('127.0.0.1', 12345))
+
+            print "Connected"
+            while True:
+                s = self.socket.recv(1000)
+                s = s.strip()
+                # print "Received"
+                if s.startswith("TICK"):
+                    table = s.split(' ')
+                    tip = table[1]
+                    # print "Adding point"
+                    self.add_point(float(tip))
+
+
+        def send(self, text):
+            print self.socket.send(str(text))
+
+        def close(self):
+            self.socket.shutdown(socket.SHUT_WR)
+            self.socket.close()
+
+    def connect_socket(self):
+        print "Connect"
+        self.serial_thread = self.UpdatePlotThread(self.add_point)
+        self.serial_thread.start()
+
+    def disconnect_socket(self):
+        print "Disconnect"
+        self.serial_thread.terminate()
+        self.serial_thread.wait()
+        self.serial_thread.close()
+
+    def send(self):
+        text = self.ui.sendText.text()
+        print "Sending: ", text
+        self.serial_thread.send(text + "\n")
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ Software/PyGUI/main_window.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_window.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.sendText = QtGui.QLineEdit(self.centralwidget)
+        self.sendText.setObjectName(_fromUtf8("sendText"))
+        self.horizontalLayout.addWidget(self.sendText)
+        self.sendButton = QtGui.QPushButton(self.centralwidget)
+        self.sendButton.setObjectName(_fromUtf8("sendButton"))
+        self.horizontalLayout.addWidget(self.sendButton)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.widget = PlotWidget(self.centralwidget)
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.verticalLayout_2.addWidget(self.widget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuConnect = QtGui.QMenu(self.menubar)
+        self.menuConnect.setObjectName(_fromUtf8("menuConnect"))
+        self.menuControl = QtGui.QMenu(self.menubar)
+        self.menuControl.setObjectName(_fromUtf8("menuControl"))
+        MainWindow.setMenuBar(self.menubar)
+        self.actionSimulator = QtGui.QAction(MainWindow)
+        self.actionSimulator.setObjectName(_fromUtf8("actionSimulator"))
+        self.actionDisconnect = QtGui.QAction(MainWindow)
+        self.actionDisconnect.setObjectName(_fromUtf8("actionDisconnect"))
+        self.menuConnect.addAction(self.actionSimulator)
+        self.menuConnect.addSeparator()
+        self.menuConnect.addAction(self.actionDisconnect)
+        self.menubar.addAction(self.menuConnect.menuAction())
+        self.menubar.addAction(self.menuControl.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.sendButton.setText(_translate("MainWindow", "Send", None))
+        self.menuConnect.setTitle(_translate("MainWindow", "Connection", None))
+        self.menuControl.setTitle(_translate("MainWindow", "Control", None))
+        self.actionSimulator.setText(_translate("MainWindow", "Simulator", None))
+        self.actionDisconnect.setText(_translate("MainWindow", "Disconnect", None))
+
+from pyqtgraph import PlotWidget

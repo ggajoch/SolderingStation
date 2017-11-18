f6c43be0fa85834186af06fdd56425d23c5de9f0
Grzegorz Gajoch <gajoch@gmail.com>
2017-11-18 21:42:42 +0000

    Enable LTO on release build

--- Firmware/targets/arm/settings.cmake
+++ Firmware/targets/arm/settings.cmake
@@ -2,10 +2,10 @@ set (CWARN "-Wall -Wstrict-prototypes -Wextra -Werror")
 set (CXXWARN "-Wall -Wextra -Werror")
 set(CTUNING "-fomit-frame-pointer -ffunction-sections -fdata-sections")
 set(CMCU "-mcpu=cortex-m3 -mthumb -mfloat-abi=soft")
-set(RANDOM_DEFS "-D\"__weak=__attribute__((weak))\" -D\"__packed=__attribute__((__packed__))\" -DUSE_HAL_DRIVER -DSTM32F103xB ")
+set(RANDOM_DEFS "-DUSE_HAL_DRIVER -DSTM32F103xB")
 
-set(CMAKE_C_FLAGS "-std=gnu11 -Os ${CWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS} -g")
-set(CMAKE_CXX_FLAGS "-std=gnu++1z -Os -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS} -g")
+set(CMAKE_C_FLAGS "-std=gnu11 ${CWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS}")
+set(CMAKE_CXX_FLAGS "-std=gnu++1z -fno-exceptions -fno-rtti ${CXXWARN} ${CTUNING} ${CMCU} ${RANDOM_DEFS}")
 set(CMAKE_CXX_STANDARD 14)
 
 set(PLATFORM_LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/STM32F103C8Tx_FLASH.ld)

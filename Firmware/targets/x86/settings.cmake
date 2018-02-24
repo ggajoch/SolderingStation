d98f979a526bcd85e3b40db0cef3ffb2deb0cd29
Grzegorz Gajoch <gajoch@gmail.com>
2018-02-25 00:16:40 +0100

    Enable C++17 features

--- Firmware/targets/x86/settings.cmake
+++ Firmware/targets/x86/settings.cmake
@@ -2,5 +2,5 @@ set(CWARN "-Wall -Wstrict-prototypes -Wextra")
 set(CXXWARN "-Wall -Wextra")
 
 set(CMAKE_C_FLAGS "-std=gnu11 ${CWARN} ${CMCU}")
-set(CMAKE_CXX_FLAGS "-std=gnu++14 ${CXXWARN} ${CMCU}")
-set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_FLAGS "-std=gnu++17 ${CXXWARN} ${CMCU}")
+set(CMAKE_CXX_STANDARD 17)
